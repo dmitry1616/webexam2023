8fdcb4b63042a105880c51f4f4163f28e531991f
dmitry1616 <dima-stoyannov@yandex.ru>
2023-01-11 15:26:02 +0300

    commit1

---
+++ src/components/QuestionList.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="question" v-for="question in questions" v-bind:key="question">
+        <div><strong>Пользователь </strong>{{ question.userId }}</div>
+        <div><strong>Вопрос №</strong> {{ question.id }}</div>
+        <div><strong>Вопрос:</strong> {{ question.title }}</div>
+        <div><strong>Комментарий:</strong>{{ question.body }}</div>
+    </div>
+
+
+</template>
+
+<script>
+export default {
+    props: {
+        questions: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.question {
+    border: solid 1px black;
+    padding: 15px;
+}
+.question div{
+    padding: 5px;
+}
+</style>
