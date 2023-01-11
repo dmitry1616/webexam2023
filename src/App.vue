2fd5c1326da0a305ab3a5c106af5a5011d42f6fa
dmitry1616 <dima-stoyannov@yandex.ru>
2023-01-11 17:21:22 +0300

    commitafternew

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <question-list  :questions="questions"/>
+        <QuestionNew @create="createQuestion" />
+        <question-list :questions="questions" />
+
+
     </div>
 </template>
 
 <script>
 import QuestionList from "@/components/QuestionList.vue"
+import QuestionNew from "@/components/QuestionNew.vue"
 export default {
     components:
     {
-        QuestionList
+        QuestionList, QuestionNew
     },
 
     data() {
@@ -17,9 +21,14 @@ export default {
             questions: [
                 { userId: 1, id: 2, title: "когда я закончу проект??", body: "ddddd" },
                 { userId: 2, id: 3, title: "когдаssss я закончу проект??", body: "ddddd" },
-                { userId: 3, id: 4, title: "ког4545роект??", body: "dddddddghd" },
-                
-            ]
+                { userId: 3, id: 4, title: "ког45dd45роект??", body: "dddddddghd" },
+
+            ],
+        }
+    },
+    methods: {
+        createQuestion(question) {
+            this.questions.push(question);
         }
     }
 }
---
+++ src/components/QuestionNew.vue
@@ -0,0 +1,43 @@
+<template>
+    <form @submit.prevent>
+        <h4>Задать вопрос</h4>
+        <input v-model="question.title" class="input" type="text" placeholder="Вопрос">
+        <input v-model="question.body" class="input" type="text" placeholder="Комментарий">
+        <button 
+        class="btn" 
+        @click="createQuestion"
+        >Отправить</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            question: {
+                title: "",
+                body: ""
+            }
+        }
+    },
+
+    methods: {
+        createQuestion() {
+            this.question.body
+            this.question.title
+            this.question.id = Date.now()
+            this.question.userId = 0
+            this.$emit("create", this.question)
+            this.question = {
+                title: "", body: ""
+            }
+            
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
